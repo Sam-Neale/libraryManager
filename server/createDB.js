5638a8ed44c4c247ed3aaa58889679192e6e0c9f
Sam Neale <sammneale@icloud.com>
2022-10-21 01:57:52 +0000

    i hope this works

---
+++ server/createDB.js
@@ -0,0 +1,20 @@
+const XMLConvert = require('xml-js');
+
+const databse = {
+    users: [
+        {
+            id: 1,
+            username: "57664",
+            password: "yes",
+            name: "Sam Neale",
+            yearLevel: 10
+        }
+    ],
+    books: [
+        {
+            id: 1,
+            title: "Ms Farrell's Chicken Extravaganza",
+            
+        }
+    ]
+}
--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@
 
 const express = require("express");
 const path = require("path");
+const XMLConvert = require('xml-js');
+const fs = require("fs");
+
+console.log(path.join(__dirname, '/public'));
 
 //Express
 const app = express();
@@ -11,8 +15,10 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, '/../', 'views'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use('/public', express.static(path.join(__dirname, '/../public')));
 app.listen(3000);
 
+//Routes
 app.get("*", (req,res)=>{
   switch(req.path){
     case "/":
@@ -22,3 +28,18 @@ app.get("*", (req,res)=>{
       res.render("404");
   }
 })
+
+app.post
+
+const demoObject = [
+  {
+    option: 1
+  },
+  {
+    option: 2
+  }
+]
+
+const XMLOptions = { compact: true, ignoreComment: true, spaces: 4 };
+const result = XMLConvert.json2xml(JSON.stringify(demoObject), XMLOptions);
+console.log(result);
